22a77d6deb23349d5dbdb1abc13d8fd14999bd54
Marccudi <45258590+Marccudi@users.noreply.github.com>
2021-03-25 20:10:00 +0100

    SOLOVISUAL

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Home/home/home.component';
+import { LibrosComponent } from './Libros/libros/libros.component';
+import { NuevoComponent } from './Libros/nuevo/nuevo.component';
+import { PedirlibroComponent } from './Libros/pedirlibro/pedirlibro.component';
+import { PerfillibroComponent } from './Libros/perfillibro/perfillibro.component';
+import { VotoComponent } from './Libros/voto/voto.component';
+import { ForgotComponent } from './Login/forgot/forgot.component';
+import { LoginComponent } from './Login/login/login.component';
+import { RegisterComponent } from './Login/register/register.component';
+import { MainComponent } from './Main/main/main.component';
+import { MislibrosComponent } from './Usuarios/mislibros/mislibros.component';
+import { PerfilComponent } from './Usuarios/perfil/perfil.component';
+
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'login'},
+  {path:'login',component:LoginComponent},
+  {path:'login/forgot',component:ForgotComponent},
+  {path:'login/register',component:RegisterComponent},
+  {path:'main',component:MainComponent},
+  {path:'home',component:HomeComponent},
+  {path:'libros',component:LibrosComponent},
+  {path:'libros/nuevo',component:NuevoComponent},
+  {path:'libros/pedirlibro',component:PedirlibroComponent},
+  {path:'libros/voto',component:VotoComponent},
+  {path:'libros/perfillibro',component:PerfillibroComponent},
+  {path:'usuario/perfil',component:PerfilComponent},
+  {path:'usuario/libros',component:MislibrosComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
